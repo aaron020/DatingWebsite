c3b9d5bef97c0898243b45d0180f37b7fdbb7ead
Aaron Meade <64416980+aaron020@users.noreply.github.com>
2022-03-11 12:54:02 +0000

    connections file

--- connections.php
+++ connections.php
@@ -1,9 +1,15 @@
 <?php
+//Local
+// $dbhost = "localhost";
+// $dbuser = "root";
+// $dbpass = "";
+// $dbname = "loveconnect";
 
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "";
-$dbname = "loveconnect";
+$dbhost = "sql108.epizy.com";
+$dbuser = "epiz_31203799";
+$dbpass = "siSrE7hjLok";
+$dbname = "epiz_31203799_loveconnect";
+
 
 if(!$con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname)){
 
