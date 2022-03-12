5b2b9aef5278c6bbb13e5efe63fe5b357b16452b
ColumMaher <55181702+ColumMaher@users.noreply.github.com>
2022-03-12 16:23:57 +0000

    Logging In
    Can now login (Seems to be an error first try, not sure why something to do with the superglobals) System checks ID and password (which is hashed). Have to edit password column to be varchar 255, the current length causes issues with the hash and password verification. :)

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -34,20 +34,44 @@ function pwdCon($pwd, $pwd_confirm) {
 }
 function UidExists($conn, $name) {
 	
-	$sql = "SELECT * FROM users WHERE usersId = ?;";
+	$sql = "SELECT * FROM users WHERE usersName = ?;";//Add OR usersEmail = ? if looking for email
 	$stmt = mysqli_stmt_init($conn);
 	if (!mysqli_stmt_prepare($stmt, $sql)) {
-		//header("location: ../Register.html?error=stmtfailed");
-		echo $conn;
+		header("location: ../Login.html?error=stmtfailed");
 		exit();
 	}
 	
 	mysqli_stmt_bind_param($stmt, "s", $name);
 	mysqli_stmt_execute($stmt);
 	
-	$resultUid = mysqli_stmt_get_result($stmt);
+	$resultData = mysqli_stmt_get_result($stmt);
 	
-	if ($row = mysqli_fetch_assoc($resultUid)) {
+	if ($row = mysqli_fetch_assoc($resultData)) {
+		return $row;
+	}
+	else {
+		$result = false;
+		return $result;
+	}
+	
+	mysqli_stmt_close($stmt);
+}
+
+function pwdGet($conn, $name) {
+	
+	$sql = "SELECT password FROM users WHERE usersName = ?;";
+	$stmt = mysqli_stmt_init($conn);
+	if (!mysqli_stmt_prepare($stmt, $sql)) {
+		header("location: ../Login.html?error=stmtfailed");
+		exit();
+	}
+	
+	mysqli_stmt_bind_param($stmt, "s", $name);
+	mysqli_stmt_execute($stmt);
+	
+	$resultData = mysqli_stmt_get_result($stmt);
+	
+	if ($row = mysqli_fetch_assoc($resultData)) {
 		return $row;
 	}
 	else {
@@ -67,11 +91,44 @@ function createUser($conn, $name, $pwd) {
 		exit();
 	}
 	
-	$hashedpwd = password_hash($pwd, PASSWORD_DEFAULT);
+	$hashedpwd = password_hash($pwd, PASSWORD_BCRYPT);
 	
 	mysqli_stmt_bind_param($stmt, "ss", $name, $hashedpwd);
 	mysqli_stmt_execute($stmt);
 	mysqli_stmt_close($stmt);
 	header("location: ../EditUser.html?error=none");
 	exit();
+}
+function noInputLogin($name, $pwd) {
+	$return;
+	if (empty($name) || empty($pwd)) {
+		$return = true;
+	}
+	else {
+		$return = false;
+	}
+	return $return;
+}
+function loginUser($conn, $name, $pwd){
+	
+	$uidExists = UidExists($conn, $name);//Add another id parameter for email if login using email
+	$passCheck = pwdGet($conn, $name);
+	
+	if ($uidExists == false) {
+		header("location: ../Login.html?error=wronglogin");
+		exit();
+	}
+	$pwdHashed = $passCheck;
+	$checkPwd = password_verify($pwd,$pwdHashed["password"]);
+	
+	if ($checkPwd == false) {
+		header("location: ../Login.html?error-wrongpassword= ");
+		exit();
+	}
+	else if ($checkPwd == true) {
+		session_start();
+		$_SESSION["userName"] = $uidExists["userName"];
+		header("location: ../EditUser.html?error=none");
+		exit();
+	}
 }
--- includes/login.inc.php
+++ includes/login.inc.php
@@ -1,1 +1,35 @@
-<?php >
+<?php 
+
+	include "dbh.inc.php";
+
+	if (isset($_POST["submit"])) {
+		
+		$name = $_POST["usersId"];
+		$pwd = $_POST["pwd"];
+		
+		require_once 'dbh.inc.php';
+		$serverName = "localhost";
+		$DBUserName = "root";
+		$dBPassword = "";
+		$dBName = "loveconnect";
+		
+		$conn = mysqli_connect($serverName, $DBUserName, $dBPassword, $dBName);
+
+		if(!$conn){
+			
+			die("Error connecting to database: " . mysqli_connect_error());
+			
+		}
+		
+		require_once 'functions.inc.php';
+		
+		if (noInputLogin($name, $pwd) != false) {
+			header("location: ../Login.html?error=emptyinput");
+			exit();
+		}
+		loginUser($conn, $name, $pwd);
+	}
+	else {
+		header("location: ../Login.html?error=dunno");
+		exit();
+	}
