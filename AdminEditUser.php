3c5375ad72ede1ba8a6cd1b07f64ffbc84174b7c
Aaron Meade <64416980+aaron020@users.noreply.github.com>
2022-04-01 12:20:43 +0100

    Admin - Edit User

--- AdminEditUser.php
+++ AdminEditUser.php
@@ -12,47 +12,58 @@ $username = getUsername($userId_LoggedIn, $con);
 ?>
 
 
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-<meta charset="utf-8"/>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="style/InputStyle.css">
-  <title><?php echo textStyle($username["username"])?> Details</title>
-</head>
-<body>
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-  <div class="box" style="width: 40%;">
-    <a href="Menu.php"> <button style="width: 20%;">Main Menu</button> </a>
-      <h1 style="font-family: arial;"><?php echo textStyle($userDet["firstname"])?>  <?php echo textStyle($userDet["lastname"])?> Details</h1>
-      <hr>
-      <!-- onsubmit="return validateForm() -->
-      
-      <form action="includes/editUser.inc.php" name="CreateUser" method = "post"
-       enctype="multipart/form-data"> 
-        <div class="userdets">
-        
-        <h3>Credentials:</h3>
-          <input type="text" name = "username" id="username"placeholder="JohnSmith123">
-          <input type="text" name = "password" id="password" placeholder="New Password">
-        </div>   
-                    
-        <div class="image-button">
-            <h3>Profile Pic:</h3>
-            <input type="file" id="img" name="img" accept="image/*">
+    <link rel="stylesheet" type="text/css" href="style/userdetails.css">
+
+    <title>Edit Details</title>
+  </head>
+  <body>
+    <div class="container ml-6">
+      <h1 class="heading">Edit User</h1>
+      <form class="row g-3" action="includes/admin_editUser.inc.php"  method = "post" enctype="multipart/form-data">
+        <div class="mt-5 col-md-12">
+          <a href="Menu.php">Return</a>
+        </div>
+
+        <div class="mt-5 col-md-6">
+          <label for="username" class="form-label">Username</label>
+          <input type="text" name= "username" class="form-control" id="username"  placeholder="<?php echo $username["username"]?>">
         </div>
 
 
-        <div class="input-user">
-          <h3>Name:</h3>
-          <input type="text" name = "firstname" id="firstname" maxlength="20" placeholder="<?php echo textStyle($userDet["firstname"])?>">
-          <input type="text" name = "lastname" id="lastname" maxlength="20" placeholder="<?php echo textStyle($userDet["lastname"])?>">
+        <div class="mt-5 col-md-6">
+          <label for="password" class="form-label">Password</label>
+          <input type="password" name="password" class="form-control" id="password"  placeholder="">
         </div>
 
-        <div class="slidecontainer">
-          <h3>Age: <span id="demo"></span></h3>
-          <input style="width: 100%" name = "age" type="range" min="18" max="99" value="<?php echo $userDet["age"]?>" class="slider" id="myRange">
+        <div class="mt-5 col-md-12 text-center">
+          <label for="profilePic">Profile Picture</label>
+          <input type="file" name="img" id="img" accept="image/*">
+        </div>
+
+        <div class="mt-5 col-md-6">
+          <label for="firstname" class="form-label">Firstname</label>
+          <input type="text" name= "firstname" class="form-control" id="firstname" maxlength="20" placeholder="<?php echo textStyle($userDet["firstname"])?>">
+        </div>
+
+
+        <div class="mt-5 col-md-6">
+          <label for="lastname" class="form-label">Lastname</label>
+          <input type="text" name="lastname" class="form-control" id="lastname" maxlength="20" placeholder="<?php echo textStyle($userDet["lastname"])?>">
+        </div>
+
+        <div class="mt-5 col-md-12">
+          <label for="age">Age : <span id="demo"></label>
+          <input type="range" class="form-control-range" name="age" id="myRange" min="18" max="99" value="<?php echo $userDet["age"]?>">
           <script>
             var slider = document.getElementById("myRange");
             var output = document.getElementById("demo");
@@ -64,40 +75,49 @@ $username = getUsername($userId_LoggedIn, $con);
           </script>
         </div>
 
-        <div class="City">
-          <h3>City:</h3>
-          <input type="text" id = "city" name="city" maxlength= "30" placeholder="<?php echo textStyle($userDet["city"])?>">
+        <div class="mt-5 col-md-6">
+          <label for="city" name="city" class="form-label">City</label>
+          <input type="text" class="form-control" id="city" name= "city" maxlength="60" placeholder="<?php echo textStyle($userDet["city"])?>">
+        </div>      
+
+        <div class="mt-5 col-md-6">
+          <label for="uni" name="uni" class="form-label">University</label>
+          <input type="text" class="form-control" id="uni" name= "uni" maxlength="60"
+          placeholder="<?php echo textStyle($userDet["university"])?>">
         </div>
 
-        <div class="Bio">
-          <h3>Bio :</h3>
-          <textarea rows="5" maxlength="250" name="bio" id = "bio" placeholder="<?php echo $userDet["bio"]?>"></textarea>
+        <div class="mt-5 col-md-12">
+          <label for="bio" name="uni" class="form-label">Bio</label>
+          <textarea type="text" class="form-control" id="bio" name= "bio" maxlength="250" placeholder="<?php echo $userDet["bio"]?>"></textarea>
         </div>
 
-        <div class="Uni">
-          <h3>University:</h3>
-          <input type="text" name = "uni" id = "uni" maxlength= "30" placeholder="<?php echo textStyle($userDet["university"])?>">
-        </div>
+        <div class="mt-5 col-md-12">
+          <label for="job" name="job" class="form-label">Job</label>
+          <input type="text" class="form-control" id="job" name= "job" maxlength="60" placeholder="<?php echo textStyle($userDet["job"])?>">
+        </div>                      
 
-        <div class="Job">
-          <h3>Job:</h3>
-          <input type="text" name  = "job" id = "job" maxlength = "30" placeholder="<?php echo textStyle($userDet["job"])?>">
-        </div>
-
-        <div class="Hobbies">
-          <h3>Hobbies:</h3>
-          <input type="text" name="hobbies" id = "hobbies" maxlength = "100" placeholder="<?php echo $userDet["hobbies"]?>">
-        </div>
-
-        <div class="Contact">
-          <h3>Instagram Handle: </h3>
-          <input type="text" name="contact" id = "contact" maxlength="30" placeholder="<?php echo $userDet["contact"]?>">
-        </div>
-
-        <div class="submit-button">
-          <button type="submit" class="float">Update Profile</button>
+        <div class="mt-5 col-md-12">
+          <label for="hobbies" name="hobbies" class="form-label">Hobbies</label>
+          <input type="text" class="form-control" id="hobbies" name= "hobbies" maxlength="100" placeholder="<?php echo $userDet["hobbies"]?>">
         </div> 
+
+
+      <div class="mt-5 col-md-12">
+        <label for="contact">Instagram</label>
+        <div class="input-group">
+          <div class="input-group-prepend">
+            <div class="input-group-text">@</div>
+          </div>
+          <input type="text" class="form-control" id="contact" name="contact" placeholder="<?php echo $userDet["contact"]?>">
+        </div>
+      </div>  
+
+      <div class="mt-5 mb-5 col-md-12 text-center">
+        <button type="submit" class="submit-button">Update Profile</button>
+      </div> 
+
       </form>
-  </div>
-</body>
+    </div>
+
+  </body>
 </html>
